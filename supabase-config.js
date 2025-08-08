f559639659deb0c3bbb369b4bc582ba728ea8fe9
Pramod Kumar <pramod.k0308@gmail.com>
2025-08-08 18:25:00 +0100

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+// supabase-config.js
+export const SUPABASE_URL = "https://mnghhnshzzhmtozixyoe.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZ2hobnNoenpobXRveml4eW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2NzE5MjQsImV4cCI6MjA3MDI0NzkyNH0.VJ0ioBYhpWrGzwy3v6Jh10Sj4-qblzZPrIF04yyYVqk";
+
